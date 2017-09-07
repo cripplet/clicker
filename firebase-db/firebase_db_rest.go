0758921ea330a4231d6dc436d1de37efc929d985
Carl Sagan <sagan@debian70.vm>
2017-09-07 05:35:40 -0500

    Add basic HTTP REST support for Firebase DB

--- firebase-db/firebase_db_rest.go
+++ firebase-db/firebase_db_rest.go
@@ -1,49 +1,107 @@
 package firebase_db
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 //  See https://firebase.google.com/docs/reference/rest/database/.
 
+func do(c *http.Client, req *http.Request) ([]byte, int, error) {
+	resp, err := c.Do(req)
+	if err != nil { return nil, 0, err }
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	return b, resp.StatusCode, err
+}
+
+func paramToURL(p map[string]string) string {
+	s := ""
+	if len(p) > 0 {
+		s += "&"
+	}
+	for k, v := range p {
+		s += fmt.Sprintf("%s=%s", k, v)
+	}
+	return s
+}
+
 func Get(
 	c *http.Client,
 	path string,
 	x_firebase_etag bool,
-	access_token string,
-	query_parameters map[string]string,
-	resp *[]byte) (int, error) {
-	return http.StatusOK, nil
+	query_parameters map[string]string) ([]byte, int, error) {
+
+	path += paramToURL(query_parameters)
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+	if err != nil { return nil, 0, err }
+
+	req.Header.Set("Content-Type", "application/json")
+	if x_firebase_etag == true {
+		req.Header.Set("X-Firebase-ETag", "true")
+	}
+
+	return do(c, req)
 }
 
 func Put(
 	c *http.Client,
 	path string,
-	req *[]byte,
+	data []byte,
 	x_firebase_etag bool,
 	if_match string,
-	query_parameters map[string]string,
-	resp *[]byte) (int, error) {
-	return http.StatusOK, nil
+	query_parameters map[string]string) ([]byte, int, error) {
+
+	path += paramToURL(query_parameters)
+	req, err := http.NewRequest(http.MethodPut, path, bytes.NewReader(data))
+	if err != nil { return nil, 0, err }
+
+	req.Header.Set("Content-Type", "application/json")
+	if x_firebase_etag == true {
+		req.Header.Set("X-Firebase-ETag", "true")
+	}
+	if if_match != "" {
+		req.Header.Set("if-match", if_match)
+	}
+
+	return do(c, req)
 }
 
 func Post(
 	c *http.Client,
 	path string,
-	req *[]byte,
+	data []byte,
 	x_firebase_etag bool,
-	query_parameters map[string]string,
-	resp *[]byte) (int, error) {
-	return http.StatusOK, nil
+	query_parameters map[string]string) ([]byte, int, error) {
+
+	path += paramToURL(query_parameters)
+	req, err := http.NewRequest(http.MethodPost, path, bytes.NewReader(data))
+	if err != nil { return nil, 0, err }
+
+	req.Header.Set("Content-Type", "application/json")
+	if x_firebase_etag == true {
+		req.Header.Set("X-Firebase-ETag", "true")
+	}
+
+	return do(c, req)
 }
 
 func Patch(
 	c *http.Client,
 	path string,
-	req *[]byte,
-	query_parameters map[string]string,
-	resp *[]byte) (int, error) {
-	return http.StatusOK, nil
+	data []byte,
+	query_parameters map[string]string) ([]byte, int, error) {
+
+	path += paramToURL(query_parameters)
+	req, err := http.NewRequest(http.MethodPatch, path, bytes.NewReader(data))
+	if err != nil { return nil, 0, err }
+
+	req.Header.Set("Content-Type", "application/json")
+
+	return do(c, req)
 }
 
 func Delete(
@@ -51,7 +109,19 @@ func Delete(
 	path string,
 	x_firebase_etag bool,
 	if_match string,
-	query_parameters map[string]string,
-	resp *[]byte) (int, error) {
-	return http.StatusOK, nil
+	query_parameters map[string]string) ([]byte, int, error) {
+
+	path += paramToURL(query_parameters)
+	req, err := http.NewRequest(http.MethodPatch, path, nil)
+	if err != nil { return nil, 0, err }
+
+	if x_firebase_etag == true {
+		req.Header.Set("X-Firebase-ETag", "true")
+	}
+	if if_match != "" {
+		req.Header.Set("if-match", if_match)
+	}
+
+	_, status_code, err := do(c, req)
+	return nil, status_code, err
 }
