f460d734ba28af598b08007e57932d9662add73d
Carl Sagan <sagan@debian70.vm>
2017-09-25 00:50:52 -0500

    Add rate limiter to REST server

--- clicker-rest/server.go
+++ clicker-rest/server.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/cripplet/clicker/clicker-rest/lib"
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
 	"net/http"
+	"time"
 )
 
+var rateLimiter *limiter.Limiter = tollbooth.NewLimiter(2, time.Second, nil)
+
 func main() {
-	http.HandleFunc("/", cc_rest_lib.GameRouter)
+	http.Handle("/", tollbooth.LimitFuncHandler(rateLimiter, cc_rest_lib.GameRouter))
 	http.ListenAndServe(":8080", nil)
 }
